370fcf4ecc8ac3e68baecd816d94eaed98576d74
Kamil Zwarycz <kzwarycz@antmicro.com>
2024-03-16 16:58:50 +0100

    Improve Python script

--- get_image_urls.py
+++ get_image_urls.py
@@ -12,18 +12,22 @@ def search_images_pixabay(api_key, search_term, count):
     response = requests.get(search_url, params=params)
     response.raise_for_status()
     search_results = response.json()
-    return [img["largeImageURL"] for img in search_results["hits"]]
+    return [
+        {
+            "id": img["id"],
+            "title": f"Image {index + 1}",
+            "url": img["largeImageURL"]
+        }
+        for index, img in enumerate(search_results["hits"])
+    ]
 
-# Parse command-line arguments
 parser = argparse.ArgumentParser(description="Retrieve image links using the Pixabay API")
 parser.add_argument("--api_key", required=True, help="Pixabay API key")
 parser.add_argument("--search_term", required=True, help="Search term for images")
-parser.add_argument("--count", type=int, default=20, help="Number of images to retrieve (default: 10)")
+parser.add_argument("--count", type=int, default=10, help="Number of images to retrieve (default: 10)")
 args = parser.parse_args()
 
-# Retrieve image links
-image_urls = search_images_pixabay(args.api_key, args.search_term, args.count)
+images_info = search_images_pixabay(args.api_key, args.search_term, args.count)
 
-# Save image links to a JSON file
-with open('image_urls.json', 'w') as f:
-    json.dump(image_urls, f)
+with open('image_data.json', 'w') as f:
+    json.dump(images_info, f, indent=4)
